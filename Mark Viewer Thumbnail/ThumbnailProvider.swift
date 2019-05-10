b86ebab7ec770928970769f45c2c2047aaf5924f
Nelson Tai <chiahsien@gmail.com>
2019-05-10 15:27:20 +0800

    Modify Podfile to prepare for Thumbnail target

---
+++ Mark Viewer Thumbnail/ThumbnailProvider.swift
@@ -0,0 +1,41 @@
+//
+//  ThumbnailProvider.swift
+//  Mark Viewer Thumbnail
+//
+//  Created by Nelson Dai on 2019/5/10.
+//  Copyright © 2019 Nelson Tai. All rights reserved.
+//
+
+import UIKit
+import QuickLook
+
+class ThumbnailProvider: QLThumbnailProvider {
+    
+    override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
+        
+        // There are three ways to provide a thumbnail through a QLThumbnailReply. Only one of them should be used.
+        
+        // First way: Draw the thumbnail into the current context, set up with UIKit's coordinate system.
+        handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing: { () -> Bool in
+            // Draw the thumbnail here.
+            
+            // Return true if the thumbnail was successfully drawn inside this block.
+            return true
+        }), nil)
+        
+        /*
+        
+        // Second way: Draw the thumbnail into a context passed to your block, set up with Core Graphics' coordinate system.
+        handler(QLThumbnailReply(contextSize: request.maximumSize, drawing: { (context) -> Bool in
+            // Draw the thumbnail here.
+         
+            // Return true if the thumbnail was successfully drawn inside this block.
+            return true
+        }), nil)
+         
+        // Third way: Set an image file URL.
+        handler(QLThumbnailReply(imageFileURL: Bundle.main.url(forResource: "fileThumbnail", withExtension: "jpg")!), nil)
+        
+        */
+    }
+}
--- Podfile
+++ Podfile
@@ -2,6 +2,9 @@ platform :ios, '11.0'
 inhibit_all_warnings!
 use_frameworks!
 
-target 'Mark Viewer' do
+abstract_target 'Common Pods' do
     pod 'EFMarkdown', '5.0.0'
+
+    target 'Mark Viewer'
+    target 'Mark Viewer Thumbnail'
 end
