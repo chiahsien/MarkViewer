8139f56ecea395b40cc8ba7195a9deeb831b0488
Nelson Tai <chiahsien@gmail.com>
2019-05-19 17:01:11 +0800

    Add ability to change syntax highlight

--- Mark Viewer/Markdown View/MarkdownView.swift
+++ Mark Viewer/Markdown View/MarkdownView.swift
@@ -10,11 +10,17 @@ import WebKit
 import EFMarkdown
 
 final class MarkdownView: WKWebView {
+    enum SyntaxHighlight: String {
+        case google = "Google Code"
+    }
+
     private let bundle: Bundle
     private lazy var baseURL: URL = {
         return self.bundle.url(forResource: "index", withExtension: "html")!
     }()
     private var markdownToHTMLString: String?
+    private var syntaxHighlight: SyntaxHighlight = .google
+
     var openURLHandler: ((URL) -> Void)?
 
     init(frame: CGRect) {
@@ -31,22 +37,30 @@ final class MarkdownView: WKWebView {
 
     func update(markdownString: String) {
         markdownToHTMLString = try? EFMarkdown().markdownToHTML(markdownString, options: [.default, .smart, .githubPreLang])
-        try? loadHTMLView(markdownToHTMLString)
+        try? loadHTMLView(markdownToHTMLString, syntaxHighlight: syntaxHighlight)
+    }
+
+    func change(syntaxHighlight: SyntaxHighlight) {
+        if self.syntaxHighlight == syntaxHighlight { return }
+        self.syntaxHighlight = syntaxHighlight
+        try? loadHTMLView(markdownToHTMLString, syntaxHighlight: syntaxHighlight)
     }
 }
 
 // MARK: - Private API
 private extension MarkdownView {
-    func loadHTMLView(_ htmlString: String?) throws {
+    func loadHTMLView(_ htmlString: String?, syntaxHighlight: SyntaxHighlight) throws {
         guard let htmlString = htmlString else { return }
-        let pageHTMLString = try htmlFromTemplate(htmlString)
+        let pageHTMLString = try htmlFromTemplate(htmlString, syntaxHighlight: syntaxHighlight)
 
         loadHTMLString(pageHTMLString, baseURL: baseURL)
     }
 
-    func htmlFromTemplate(_ htmlString: String) throws -> String {
-        let template = try String(contentsOf: baseURL, encoding: .utf8)
-        return template.replacingOccurrences(of: "${PLACEHOLDER}$", with: htmlString)
+    func htmlFromTemplate(_ htmlString: String, syntaxHighlight: SyntaxHighlight) throws -> String {
+        var template = try String(contentsOf: baseURL, encoding: .utf8)
+        template = template.replacingOccurrences(of: "${SYNTAX_HIGHLIGHT}$", with: syntaxHighlight.rawValue)
+        template = template.replacingOccurrences(of: "${BODY}$", with: htmlString)
+        return template
     }
 }
 
